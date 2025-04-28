295fae339b73eba4ed7320160b75a5d33e7e321e
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-28 22:06:52 +0530

    Add files via upload

---
+++ 3 OCT/3 OCT/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace _3_OCT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.employee = new System.Windows.Forms.TextBox();
+            this.emp_firstname = new System.Windows.Forms.TextBox();
+            this.emp_secondname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employeee ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 87);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(201, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EmployeeFname";
+            // 
+            // employee
+            // 
+            this.employee.Location = new System.Drawing.Point(406, 38);
+            this.employee.Name = "employee";
+            this.employee.Size = new System.Drawing.Size(100, 22);
+            this.employee.TabIndex = 2;
+            this.employee.TextChanged += new System.EventHandler(this.employee_TextChanged);
+            // 
+            // emp_firstname
+            // 
+            this.emp_firstname.Location = new System.Drawing.Point(406, 94);
+            this.emp_firstname.Name = "emp_firstname";
+            this.emp_firstname.Size = new System.Drawing.Size(100, 22);
+            this.emp_firstname.TabIndex = 3;
+            // 
+            // emp_secondname
+            // 
+            this.emp_secondname.Location = new System.Drawing.Point(406, 151);
+            this.emp_secondname.Name = "emp_secondname";
+            this.emp_secondname.Size = new System.Drawing.Size(100, 22);
+            this.emp_secondname.TabIndex = 4;
+            this.emp_secondname.TextChanged += new System.EventHandler(this.emp_secondname_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS PGothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(101, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "EmployeeLname";
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_insert.Location = new System.Drawing.Point(183, 251);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(134, 57);
+            this.btn_insert.TabIndex = 6;
+            this.btn_insert.Text = "Insert";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Location = new System.Drawing.Point(406, 252);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(128, 57);
+            this.btn_delete.TabIndex = 7;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(95, 340);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 224);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 576);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.emp_secondname);
+            this.Controls.Add(this.emp_firstname);
+            this.Controls.Add(this.employee);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox employee;
+        private System.Windows.Forms.TextBox emp_firstname;
+        private System.Windows.Forms.TextBox emp_secondname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
