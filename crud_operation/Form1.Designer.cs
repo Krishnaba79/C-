be5496262052a8c46161f6b2e09a4f5f51905321
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-29 12:23:45 +0530

    Add files via upload

---
+++ crud_operation/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace crud_operation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.id_txt = new System.Windows.Forms.TextBox();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.salary_txt = new System.Windows.Forms.TextBox();
+            this.department_txt = new System.Windows.Forms.TextBox();
+            this.insert = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departmentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeDataSet = new crud_operation.EmployeeDataSet();
+            this.label5 = new System.Windows.Forms.Label();
+            this.age_txt = new System.Windows.Forms.TextBox();
+            this.employeTableAdapter = new crud_operation.EmployeeDataSetTableAdapters.employeTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "salary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "department";
+            // 
+            // id_txt
+            // 
+            this.id_txt.Location = new System.Drawing.Point(261, 60);
+            this.id_txt.Name = "id_txt";
+            this.id_txt.Size = new System.Drawing.Size(100, 26);
+            this.id_txt.TabIndex = 4;
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(261, 100);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(100, 26);
+            this.name_txt.TabIndex = 5;
+            // 
+            // salary_txt
+            // 
+            this.salary_txt.Location = new System.Drawing.Point(261, 169);
+            this.salary_txt.Name = "salary_txt";
+            this.salary_txt.Size = new System.Drawing.Size(100, 26);
+            this.salary_txt.TabIndex = 6;
+            // 
+            // department_txt
+            // 
+            this.department_txt.Location = new System.Drawing.Point(261, 209);
+            this.department_txt.Name = "department_txt";
+            this.department_txt.Size = new System.Drawing.Size(100, 26);
+            this.department_txt.TabIndex = 7;
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(459, 62);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(75, 40);
+            this.insert.TabIndex = 8;
+            this.insert.Text = "insert";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(586, 60);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 40);
+            this.update.TabIndex = 9;
+            this.update.Text = "update";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(531, 138);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 40);
+            this.delete.TabIndex = 10;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.salaryDataGridViewTextBoxColumn,
+            this.departmentDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.employeBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 258);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 150);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "age";
+            this.ageDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            this.ageDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // salaryDataGridViewTextBoxColumn
+            // 
+            this.salaryDataGridViewTextBoxColumn.DataPropertyName = "salary";
+            this.salaryDataGridViewTextBoxColumn.HeaderText = "salary";
+            this.salaryDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.salaryDataGridViewTextBoxColumn.Name = "salaryDataGridViewTextBoxColumn";
+            this.salaryDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // departmentDataGridViewTextBoxColumn
+            // 
+            this.departmentDataGridViewTextBoxColumn.DataPropertyName = "department";
+            this.departmentDataGridViewTextBoxColumn.HeaderText = "department";
+            this.departmentDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.departmentDataGridViewTextBoxColumn.Name = "departmentDataGridViewTextBoxColumn";
+            this.departmentDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // employeBindingSource
+            // 
+            this.employeBindingSource.DataMember = "employe";
+            this.employeBindingSource.DataSource = this.employeeDataSet;
+            // 
+            // employeeDataSet
+            // 
+            this.employeeDataSet.DataSetName = "EmployeeDataSet";
+            this.employeeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "age";
+            // 
+            // age_txt
+            // 
+            this.age_txt.Location = new System.Drawing.Point(261, 135);
+            this.age_txt.Name = "age_txt";
+            this.age_txt.Size = new System.Drawing.Size(100, 26);
+            this.age_txt.TabIndex = 13;
+            // 
+            // employeTableAdapter
+            // 
+            this.employeTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 450);
+            this.Controls.Add(this.age_txt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.department_txt);
+            this.Controls.Add(this.salary_txt);
+            this.Controls.Add(this.name_txt);
+            this.Controls.Add(this.id_txt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox id_txt;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox salary_txt;
+        private System.Windows.Forms.TextBox department_txt;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox age_txt;
+        private EmployeeDataSet employeeDataSet;
+        private System.Windows.Forms.BindingSource employeBindingSource;
+        private EmployeeDataSetTableAdapters.employeTableAdapter employeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salaryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departmentDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ crud_operation/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace crud_operation
+{
+    public partial class Form1 : Form
+    {
+        Employee emp = new Employee();
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = emp.Getemploye();
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    // TODO: This line of code loads data into the 'employeeDataSet.employe' table. You can move, or remove it, as needed.
+        //    this.employeTableAdapter.Fill(this.employeeDataSet.employe);
+
+        //}
+
+        class Employee
+        {
+            private static String mycon = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+            public int id { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+            public int salary { get; set; }
+            public string department { get; set; }
+
+            private const string SelectQuery = "select * from employe";
+            private const string InsertQuery = "insert into employe(id,name,age,salary,department)values(@id,@name,@age,@salary,@department)";
+            private const string UpadteQuery = "update employe set name=@name,age=@age,salary=@salary,department=@department where id=@id";
+            private const string DeleteQuery = "delete from employe where id=@id";
+
+            public DataTable Getemploye()
+            {
+                var datatable = new DataTable();
+                using (SqlConnection con = new SqlConnection(mycon))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(SelectQuery, con))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            adapter.Fill(datatable);
+                        }
+
+                    }
+                }
+                return datatable;
+            }
+
+            public bool InsertEmployeDetails(Employee emp)
+            {
+                int rows;
+                using (SqlConnection con = new SqlConnection(mycon))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(InsertQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", emp.id);
+                        cmd.Parameters.AddWithValue("@name", emp.name);
+                        cmd.Parameters.AddWithValue("@age", emp.age);
+                        cmd.Parameters.AddWithValue("@salary", emp.salary);
+                        cmd.Parameters.AddWithValue("@department", emp.department);
+                        rows = cmd.ExecuteNonQuery();
+
+
+                    }
+                   
+                }
+
+                return (rows > 0) ? true : false;
+            }
+
+            public bool UpdateEmployeDetails(Employee emp)
+            {
+                int rows;
+                using (SqlConnection con = new SqlConnection (mycon))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(UpadteQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id",emp.id);
+                        cmd.Parameters.AddWithValue("@name", emp.name);
+                        cmd.Parameters.AddWithValue("@age", emp.age);
+                        cmd.Parameters.AddWithValue("@salary", emp.salary);
+                        cmd.Parameters.AddWithValue("@department", emp.department);
+                        rows = cmd.ExecuteNonQuery();
+                    }
+                }
+                return (rows>0) ? true : false;
+            }
+
+            public bool DeleteEmployeDetails(Employee emp)
+            {
+                int rows;
+                using (SqlConnection con = new SqlConnection(mycon))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(DeleteQuery,con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", emp.id);
+                        rows = cmd.ExecuteNonQuery();
+                    }
+
+                }
+                return(rows > 0)? true:false;
+            }
+            
+        }
+
+        private void insert_Click(object sender, EventArgs e)//insert
+        {
+            if (int.TryParse(id_txt.Text, out int id) &&
+                int.TryParse(age_txt.Text, out int age) &&
+                 int.TryParse(salary_txt.Text, out int salary))
+            {
+                emp.id = id;
+                emp.name = name_txt.Text;
+                emp.age = age;
+                emp.salary = salary;
+                emp.department = department_txt.Text;
+
+                var success = emp.InsertEmployeDetails(emp);
+                dataGridView1.DataSource = emp.Getemploye();
+
+                if (success)
+                {
+                    id_txt.Text = "";
+                    name_txt.Text = "";
+                    age_txt.Text = "";
+                    salary_txt.Text = "";
+                    department_txt.Text = "";
+                    MessageBox.Show("Data inserted successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Data is not inserted");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numeric values for ID, Age, and Salary.");
+            }
+
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)//delete
+        {
+            if (int.TryParse(id_txt.Text, out int id))
+            {
+                emp.id = id;
+                var success = emp.DeleteEmployeDetails(emp);
+                dataGridView1.DataSource = emp.Getemploye();
+
+                if (success)
+                {
+                    id_txt.Text = "";
+                    name_txt.Text = "";
+                    age_txt.Text = "";
+                    salary_txt.Text = "";
+                    department_txt.Text = "";
+                    MessageBox.Show("Employee deleted");
+                }
+                else
+                {
+                    MessageBox.Show("Employee not deleted");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid numeric value for ID.");
+            }
+
+        }
+
+        private void update_Click(object sender, EventArgs e)//update
+        {
+            if (int.TryParse(id_txt.Text, out int id) &&
+    int.TryParse(age_txt.Text, out int age) &&
+    int.TryParse(salary_txt.Text, out int salary))
+            {
+                emp.id = id;
+                emp.name = name_txt.Text;
+                emp.age = age;
+                emp.salary = salary;
+                emp.department = department_txt.Text;
+
+                var success = emp.UpdateEmployeDetails(emp);
+                dataGridView1.DataSource = emp.Getemploye();
+
+                if (success)
+                {
+                    MessageBox.Show("Employee has been updated");
+                }
+                else
+                {
+                    MessageBox.Show("Employee details have not been updated");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numeric values for ID, Age, and Salary.");
+            }
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)//grid
+        {
+            var index = e.RowIndex;
+            id_txt.Text = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            name_txt.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            age_txt.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            salary_txt.Text = dataGridView1.Rows[index].Cells[3].Value.ToString();
+            department_txt.Text = dataGridView1.Rows[index].Cells[4].Value.ToString();
+        }
+    }
+}
