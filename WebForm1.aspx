4abaccbf957f5a87815f113570a256378d7c65d0
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-29 13:19:13 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblName" runat ="server" Text="Enter First Number"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <asp:Label ID="lblName1" runat="server" Text="Enter Second Number"></asp:Label>
+            <asp:TextBox ID="txtName1" runat="server"></asp:TextBox>
+
+            <asp:DropDownList ID="ddl" runat="server">
+                <asp:ListItem Value="1">Addition</asp:ListItem>
+                <asp:ListItem Value="2">Submition</asp:ListItem>
+                <asp:ListItem Value="3">Multiplication</asp:ListItem>
+                <asp:ListItem Value="4">Divison</asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using Antlr.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                if (Session["number1"]!=null && Session["number2"]!=null)
+                {
+                    txtName.Text = Session["number1"].ToString();   
+                    txtName.ReadOnly = true;
+
+                    txtName1.Text = Session["number1"].ToString();
+                    txtName1.ReadOnly = true;
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            Session["number1"]=txtName.Text;
+            Session["number2"]=txtName1.Text;
+
+            txtName.Enabled = false;
+            txtName1.Enabled = false;
+            ddl.Enabled = false;
+            btnSubmit.Enabled = false;
+
+            int number1 = Convert.ToInt32(txtName.Text);
+            int number2 = Convert.ToInt32(txtName1.Text);
+
+            string option = ddl.SelectedValue;
+            switch (option)
+            {
+                case "1":
+                    result = number1 + number2;
+                    Console.WriteLine(result);
+                    break;
+                case "2":
+                    result = number1 - number2;
+                    Console.WriteLine(result);
+                    break;
+                case "3":
+                    result = number1 / number2;
+                    Console.WriteLine(result);
+                    break;
+                case "4":
+                    result = number1 * number2;
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    lblResult.Text = "InvalidcOperation";
+                    return;
+            }
+            lblResult.Text = "Result : " + result.ToString();
+
+
+        }
+
+    }
+}
