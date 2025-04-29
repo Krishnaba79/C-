9faf1fc63c7526ef5e7ed9835db734a8a50b9fa1
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-04-29 12:31:53 +0530

    Add files via upload

---
+++ Encapsulation/Encapsulation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Encapsulation
+    {
+        private string emp_name;
+        private int emp_age;
+
+        public string name
+        {
+            get { return emp_name; }
+            set { emp_name = value; }
+        }
+        public int age
+        {
+            get { return emp_age; }
+            set { emp_age = value; }
+        }
+    }
+    class emp_main
+    {
+        public static void Main(string[] args)
+        {
+            Encapsulation e1 = new Encapsulation();
+            e1.name = "Rohit";
+            e1.age = 30;
+            Console.WriteLine("Employee name is: " + e1.name);
+            Console.WriteLine("Employee age is :" + e1.age);
+        }
+    }
+}
---
+++ FirstCode/Calc1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCode
+{
+
+    internal class Calc1
+    {
+        static void Main(String[] args)
+        {
+        int a, b;
+        Console.WriteLine("Making a Calculator Program");
+        Console.WriteLine("----------------");
+
+            Console.WriteLine("Enter the value of a");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of a");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter user choice");
+            Console.WriteLine("\t1 Addition");
+            Console.WriteLine("\t2 Multiplication");
+            Console.WriteLine("\t3 Division");
+            Console.WriteLine("\t4 Substraction");
+
+            switch (Console.ReadLine())
+            {
+
+                case "1":
+                    Console.WriteLine($"The addition of the number {a} + {b} =" + (a + b));
+                    break;
+
+                case "2":
+                    Console.WriteLine($"The Multiplication of the number {a} * {b} =" + (a * b));
+                    break;
+
+                case "3":
+                    Console.WriteLine($"The Division of the number {a} / {b} =" + (a / b));
+                    break;
+
+                case "4":
+                    Console.WriteLine($"The Substraction of the number {a} - {b} =" + (a - b));
+                    break;
+
+
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
+        }
+    }
+}
